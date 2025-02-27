2264c33bd0d1d1e1e6d88589eb59f84d6837a85e
Antonio Neto <antonio.enge200@gmail.com>
2025-02-27 13:07:17 -0300

    alguns metodos

---
+++ Metodos.py
@@ -0,0 +1,56 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def iniciar_driver():
+    chrome_options = Options()
+    # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
+
+    arguments = ['--lang=pt-BR', '--window-size=800,600',
+                '--incognito']
+    ''' Common arguments
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-us , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    caminho_padrao_para_download = 'E:\\Storage\\Desktop'
+
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        # Atualiza diretório para diretório passado acima
+        'download.directory_upgrade': True,
+        # Setar se o navegar deve pedir ou não para fazer download
+        'download.prompt_for_download': False,
+        "profile.default_content_setting_values.notifications": 2,  # Desabilita notificações
+        # Permite realizar múltiplos downlaods multiple downloads
+        "profile.default_content_setting_values.automatic_downloads": 1,
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+driver = iniciar_driver()
+driver.get("https://cursoautomacao.netlify.com/")  # navegar até um site
+driver.maximize_window()  # maximizar a janela
+driver.refresh()  # recarrega página atual
+driver.get(driver.current_url)  # recarrega página atual
+driver.back()  # volta à página anterior
+driver.forward()  # navega 1 vez para frente
+print(driver.title)  # Obtem título da página
+print(driver.current_url)  # Obtem URL(endereço) da página atual
+print(driver.page_source)  # Obtem o código fonte da página atual
+# obtem o texto dentro de um elemento
+print(driver.find_element(By.XPATH, '//a[@class="navbar-brand"]').text)
+print(driver.find_element(By.XPATH, '//a[@class="navbar-brand"]').get_attribute("style"))
+
+driver.close() # Fecha janela atual
+input('')
