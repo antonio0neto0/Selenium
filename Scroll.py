e4171107157342452162ed7ef8a7e795361ecc04
Antonio Neto <antonio.enge200@gmail.com>
2025-02-28 16:09:51 -0300

    Como Usar o Scroll

---
+++ Scroll.py
@@ -0,0 +1,58 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
+
+    arguments = ['--lang=pt-BR', '--window-size=800,600',
+                '--incognito']
+    ''' Common arguments
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-us , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    caminho_padrao_para_download = 'E:\\Storage\\Desktop'
+
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        # Atualiza diretório para diretório passado acima
+        'download.directory_upgrade': True,
+        # Setar se o navegar deve pedir ou não para fazer download
+        'download.prompt_for_download': False,
+        "profile.default_content_setting_values.notifications": 2,  # Desabilita notificações
+        # Permite realizar múltiplos downlaods multiple downloads
+        "profile.default_content_setting_values.automatic_downloads": 1,
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app')
+# Rolar até o fim da página
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+sleep(5)
+# Rolar até o topo da página
+driver.execute_script("window.scrollTo(0, document.body.scrollTop)")
+sleep(5)
+# Rolar X quantidade em pixels(descer)
+driver.execute_script("window.scrollTo(0, 1500);")
+sleep(5)
+# Rolar X quantidade em pixels(subir)
+driver.execute_script("window.scrollTo(0, -1500);")
+
+input('')
+driver.close()
