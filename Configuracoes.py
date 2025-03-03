f2b0df79647e97008de36d69ca24aed405db5a08
Antonio Neto <antonio.enge200@gmail.com>
2025-03-03 13:49:25 -0300

    Configurações Padrão

--- Configuracoes.py
+++ Configuracoes.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 def iniciar_driver():
     chrome_options = Options()
